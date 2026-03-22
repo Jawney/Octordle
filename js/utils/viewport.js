c20077fae9db58b9eeb3c92b95938e0808be49b8
John Dawson <johndawson@Johns-MacBook-Air.local>
2026-03-22 13:08:10 -0400

    Fixing layout

--- js/utils/viewport.js
+++ js/utils/viewport.js
@@ -1,17 +1,6 @@
 export function updateViewport() {
-    const v = window.visualViewport;
-    const shell = document.getElementById('app-shell');
-    if (shell) {
-        shell.style.height = v.height + "px";
-        
-        // On iOS, if the keyboard is open (v.offsetTop > 0), 
-        // we move the shell to match the viewport's offset.
-        if (v.offsetTop > 0) {
-            shell.style.top = v.offsetTop + "px";
-        } else {
-            shell.style.top = "0";
-        }
-    }
+    // Only prevent the OS from scrolling when the keyboard opens.
+    // We no longer manually resize the shell or move it.
     window.scrollTo(0, 0);
 }
 
